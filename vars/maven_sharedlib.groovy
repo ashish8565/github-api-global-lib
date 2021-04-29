32759356695b80b57d08a3eee64b745cdf643718
ashish8565 <asheeshy0@gmail.com>
2021-04-30 02:40:10 +0530

    Update maven_sharedlib.groovy

--- vars/maven_sharedlib.groovy
+++ vars/maven_sharedlib.groovy
@@ -10,24 +10,7 @@ pipeline {
                 sh '''
                     echo "PATH = ${PATH}"
                     echo "M2_HOME = ${M2_HOME}"
-                '''
             }
         }
 
-        stage ('Build') {
-            steps {
-                sh 'mvn -Dmaven.test.failure.ignore=true clean install'.
-            }
-            post {
-                success {
-                    junit 'target/surefire-reports/*.xml'.
-
-                    jacoco classPattern: '**/target/classes',
-                           execPattern: '**/target/coverage-reports/jacoco-ut.exec',
-                           sourcePattern: '**/src/org/yourcompany',
-                           exclusionPattern: '**/target/classes/*closure*.class'
-                }
-            }
-        }
-    }
-}
+        
