30b1743662fb7222f3f40600d5250a3794523e64
ashish8565 <asheeshy0@gmail.com>
2021-04-30 11:21:24 +0530

    Update maven_sharedlib.groovy

--- vars/maven_sharedlib.groovy
+++ vars/maven_sharedlib.groovy
@@ -1,15 +1,25 @@
 pipeline {
     agent any
-
-    tools {
-        maven 'Maven 3.6.3'
+    tools { 
+        maven 'Maven 3.6.3' 
+        jdk 'jdk8' 
     }
     stages {
         stage ('Initialize') {
             steps {
+                sh '''
                     echo "PATH = ${PATH}"
                     echo "M2_HOME = ${M2_HOME}"
+                ''' 
             }
         }
+
+        stage ('Build') {
+            steps {
+                echo 'This is a minimal pipeline.'
+            }
+        }
+    }
+}
 
         
